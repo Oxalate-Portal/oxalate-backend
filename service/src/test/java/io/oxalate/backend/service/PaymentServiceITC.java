156447933b0fe7f96887d9137670632eae8b8cc9
Paul-Erik Törrönen <poltsi@poltsi.fi>
2025-02-01 23:02:24 +0200

    167: Set profile for ITC to test
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

--- service/src/test/java/io/oxalate/backend/service/PaymentServiceITC.java
+++ service/src/test/java/io/oxalate/backend/service/PaymentServiceITC.java
@@ -11,7 +11,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PaymentServiceITC extends AbstractIntegrationTest {
 
