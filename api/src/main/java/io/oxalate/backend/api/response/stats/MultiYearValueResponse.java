7fa3c695100ccf612d8f7fb4ef8ebfb86e6e6260
Paul-Erik Törrönen <poltsi@poltsi.fi>
2026-01-04 19:23:17 +0200

    Add aggregate stats
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

--- api/src/main/java/io/oxalate/backend/api/response/stats/MultiYearValueResponse.java
+++ api/src/main/java/io/oxalate/backend/api/response/stats/MultiYearValueResponse.java
@@ -1,5 +1,6 @@
 package io.oxalate.backend.api.response.stats;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +11,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MultiYearValueResponse {
+    @JsonProperty("year")
     private long year;
+    @JsonProperty("value")
     private long value;
+    @JsonProperty("type")
     private String type;
 }
